61b4ebfa588adfa902f7b1e29503e539a941bc17
Mahsa Shafaei <mahsa@Mahsas-MacBook-Pro.local>
2024-05-28 13:38:19 +0330

    add delete icon and dynamic labels

--- src/features/ballot/ballotAllocation/ProjectItem.tsx
+++ src/features/ballot/ballotAllocation/ProjectItem.tsx
@@ -2,23 +2,25 @@ import React from "react";
 import { useDrag } from "react-dnd";
 import { type Attestation } from "~/utils/fetchAttestations";
 
-const ProjectItem = React.memo(({ project }: { project: { name: string } }) => {
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: "ITEM",
-    item: project,
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  }));
+const ProjectItem = React.memo(
+  ({ project }: { project: { id: number; name: string } }) => {
+    const [{ isDragging }, drag] = useDrag(() => ({
+      type: "ITEM",
+      item: project,
+      collect: (monitor) => ({
+        isDragging: monitor.isDragging(),
+      }),
+    }));
 
-  return (
-    <div
-      ref={drag}
-      className={`${isDragging ? " opacity-50" : " opacity-100"} rounded-lg border border-onPrimary-light px-3 py-2 text-sm font-medium`}
-    >
-      {project.name}
-    </div>
-  );
-});
+    return (
+      <div
+        ref={drag}
+        className={`${isDragging ? " opacity-50" : " opacity-100"} rounded-lg border border-onPrimary-light px-3 py-2 text-sm font-medium`}
+      >
+        {project.name}
+      </div>
+    );
+  },
+);
 
 export default ProjectItem;
