eab82a65643d606e62425e9f79543cdb02bdb40e
Fatemeh Samie <samiefatemeh99@gmail.com>
2024-06-06 12:34:44 +0330

    Add sorting To next button

--- src/features/projects/components/NextProjectButton.tsx
+++ src/features/projects/components/NextProjectButton.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Button } from "~/components/ui/Button";
+import { useFilter } from "~/features/filter/hooks/useFilter";
 
 export const NextProjectButton = ({
   nextProjectId,
@@ -8,10 +9,12 @@ export const NextProjectButton = ({
   nextProjectId: string;
   isAdmin?: boolean;
 }) => {
+  const { orderBy, sortOrder, search } = useFilter();
+
   if (!isAdmin) return null;
   return (
     <div className="ml-2">
-      <Link href={`/projects/${nextProjectId}`}>
+      <Link href={`/projects/${nextProjectId}?search=${search}&orderBy=${orderBy}&sortOrder=${sortOrder}`}>
         <Button
           variant="primary"
           className="h-auto w-full px-6 py-[0.625rem] text-sm font-medium md:w-auto"
