d67d7fcc8c5af521563eb0b552d115960fdc4c37
Mahsa Shafaei <mahsa@Mahsas-MacBook-Pro.local>
2024-06-11 15:02:24 +0330

    fix problem of showing revoked projects

--- src/pages/projects/[projectId]/index.tsx
+++ src/pages/projects/[projectId]/index.tsx
@@ -1,6 +1,6 @@
 import { type GetServerSideProps } from "next";
 import { useAccount } from "wagmi";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 
 import { Layout } from "~/layouts/DefaultLayout";
 import ProjectDetails from "~/features/projects/components/ProjectDetails";
@@ -18,6 +18,7 @@ import { useProjectMetadata } from "~/features/projects/hooks/useProjects";
 import { Dialog } from "~/components/ui/Dialog";
 // FIXME: remove direct page import here
 import Ballot from "~/pages/ballot";
+import { useApprovedApplications } from "~/features/applications/hooks/useApprovedApplications";
 
 export default function ProjectDetailsPage({ projectId = "" }) {
   const project = useProjectById(projectId);
@@ -29,6 +30,16 @@ export default function ProjectDetailsPage({ projectId = "" }) {
   const ids = useProjectsIds();
   const [nextProjectId, setNextProjectId] = useState<string | undefined>(
     undefined,
+  );
+  const approved = useApprovedApplications();
+
+  const approvedById = useMemo(
+    () =>
+      approved.data?.reduce(
+        (map, x) => (map.set(x.refUID, true), map),
+        new Map<string, boolean>(),
+      ),
+    [approved.data],
   );
 
   useEffect(() => {
@@ -76,25 +87,38 @@ export default function ProjectDetailsPage({ projectId = "" }) {
         </div>
       }
     >
-      <ProjectDetails
-        projectMetadata={metadata?.data}
-        isLoading={metadata?.isPending}
-        isAdmin={isAdmin}
-        address={address}
-        attestation={project.data}
-        action={action}
-        state={state}
-      />
-      <DiscussionComponent
-        projectId={projectId}
-        state={state}
-        address={address}
-        isAdmin={isAdmin}
-      />
-      <Dialog size="md" isOpen={isOpen} onOpenChange={setOpen} title={`Ballot`}>
-        {/* FIXME: Ballot Page should be refactored here also modify the name */}
-        <Ballot projectName={project?.data?.name} isModal />
-      </Dialog>
+      {!project?.data?.revoked && approvedById?.has(projectId) ? (
+        <>
+          <ProjectDetails
+            projectMetadata={metadata?.data}
+            isLoading={metadata?.isPending}
+            isAdmin={isAdmin}
+            address={address}
+            attestation={project.data}
+            action={action}
+            state={state}
+          />
+          <DiscussionComponent
+            projectId={projectId}
+            state={state}
+            address={address}
+            isAdmin={isAdmin}
+          />
+          <Dialog
+            size="md"
+            isOpen={isOpen}
+            onOpenChange={setOpen}
+            title={`Ballot`}
+          >
+            {/* FIXME: Ballot Page should be refactored here also modify the name */}
+            <Ballot projectName={project?.data?.name} isModal />
+          </Dialog>
+        </>
+      ) : (
+        <p className="mt-10 flex items-center justify-center text-center">
+          Your project has been revoked.
+        </p>
+      )}
     </Layout>
   );
 }
